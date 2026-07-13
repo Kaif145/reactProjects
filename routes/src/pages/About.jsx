aad62e62552979f742efc0419a6d6597a6a4bca6
Kaifur Rahaman <kaifur.rahaman.145@gmail.com>
2026-07-13 21:35:59 +0530

    Add routing and new pages for Home, About, and Cart

---
+++ routes/src/pages/About.jsx
@@ -0,0 +1,9 @@
+export function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This page provides information about our application and its purpose.</p>
+    </div>
+  );
+}
+export default About;
