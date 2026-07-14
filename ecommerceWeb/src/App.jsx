27afe7943bcc9d5497215239fc5def9cc49ba2a2
Kaifur Rahaman <kaifur.rahaman.145@gmail.com>
2026-07-14 23:53:01 +0530

    Add initial project structure and implement basic pages
    - Created index.html, orders.html, and tracking.html with responsive design.
    - Added CSS styles for checkout, orders, and tracking pages.
    - Included general styles for buttons, links, and layout.
    - Added images for ratings and products.
    - Configured Vite for React project setup.

---
+++ ecommerceWeb/src/App.jsx
@@ -0,0 +1,18 @@
+import {Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
